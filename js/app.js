78e2685c3b112515c990cbfc8962d840ed4ffedf
robinjulio00004 <robinjulio004@gmail.com>
2024-08-07 21:07:50 -0600

    v3.3.3

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/Build.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/SourceCode.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
